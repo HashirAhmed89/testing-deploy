6943662e85fb80657c824f4eee3ed922ebf030b2
Talal <mohammadsaadali786.92@gmail.com>
2025-07-20 13:58:22 +0500

    update

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ fetch(`https://${process.env.DATABASE_URL}`, {
   },
 });
 asdad;
+
+fetch(`https://${process.env.DATABASE_URL}`, {
+  method: "post",
+  headers: {
+    apikey: "23455672421425",
+  },
+});
+asdad;
